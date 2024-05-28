c1f5c5c458bf2e2c4aacdbd2232c53807db4ba78
nghosh24 <147370977+nghosh24@users.noreply.github.com>
2024-05-27 19:06:35 -0700

    Update Intro.svelte

--- src/Components/Intro.svelte
+++ src/Components/Intro.svelte
@@ -3,13 +3,12 @@
 
 <section>
   <p class="body-text">
-    Lorem ipsum dolor sit amet <a href="">random link</a> adipisicing elit. Esse
-    dolor nobis
-    <span class="bold">bold text</span> eius voluptate distinctio odit ut quod illo!
-    Nulla consequatur fugiat quia commodi aspernatur necessitatibus est doloremque
-    quam vitae dolorem. Vitae natus ipsum animi officiis et quis obcaecati in molestiae
-    aperiam, perferendis odio a ipsam nam suscipit laboriosam corporis excepturi
-    quisquam? Ratione delectus repellat error maxime quo beatae ullam, quod
+    Climate change is one of the most pressing global problems, resulting in higher
+    temperates and more extreme weather events across the entire planet. One of the most
+    damaging weather events is hurricanes, which can destroy people's homes, livelihoods,
+    and take countless lives. We decided to do an investigation into the trends of 
+    hurricanes in North America from 2000 to 2023 to better understand the characteristics
+    and impacts of the most damaging hurricanes.
   </p>
   <p class="body-text">This is a trr</p>
 </section>
